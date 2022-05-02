2cf46fae1a4fe84cfbee03927eca97f112e01f55
Samarth Tripathi <45966581+SamarthTripathi-Design@users.noreply.github.com>
2022-05-02 06:51:21 +0530

    Grocery Bud

--- GroceyBud/src/App.js
+++ GroceyBud/src/App.js
@@ -2,9 +2,18 @@ import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else {
+    return [];
+  }
+};
+
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({
@@ -20,9 +29,18 @@ function App() {
       showAlert(true, "danger", "Please enter value");
     } else if (name && isEditing) {
       // deal with edit
-      setList(list.map((item)=>{
-        return item
-      }))
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName("");
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, "success", "value change");
     } else {
       // show alert
       showAlert(true, "success", "Item added to the list");
@@ -52,6 +70,10 @@ function App() {
     setEditID(id);
     setName(specificItem.title);
   };
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
 
   return (
     <>
