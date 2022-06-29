03fa3f38e5214963864aafbc414203aa707ce31f
SamarthTripathi-Design <tripathi.samarth@gmail.com>
2022-06-29 11:32:44 +0530

    stripe submenu

---
+++ Stripe-submenu/src/Hero.js
@@ -0,0 +1,29 @@
+import React from "react";
+import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
+
+const Hero = () => {
+  const { closeSubmenu } = useGlobalContext();
+
+  return (
+    <section className="hero" onMouseOver={closeSubmenu}>
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est culpa
+            fugiat veritatis explicabo, molestias iusto aspernatur minima cumque
+            esse. Maiores vel eveniet, quidem perferendis voluptatum praesentium
+            enim incidunt explicabo illo.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
