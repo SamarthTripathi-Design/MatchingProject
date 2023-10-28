f6f7713c38d43d7c36c82df8c2f8f71002819d4f
SamarthTripathi-Design <tripathi.samarth@gmail.com>
2023-10-28 14:27:12 +0530

    chnages in mixmaster

---
+++ mixmaster/src/App.jsx
@@ -0,0 +1,40 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  About,
+  HomeLayout,
+  Landing,
+  Error,
+  Newsletter,
+  Cocktail,
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "cocktail",
+        element: <Cocktail />,
+      },
+      {
+        path: "newsletter",
+        element: <Newsletter />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+export default App;
